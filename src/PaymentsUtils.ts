632de7e6ca32a4c2f1d264bc56ec30531e1e4f65
James Simpson <james.simpson@securetrading.com>
2020-01-26 13:40:19 +0000

    Clean-up main module

--- src/PaymentsUtils.ts
+++ src/PaymentsUtils.ts
@@ -1,6 +1,6 @@
 import Lookup from './lookup';
 import validation from './validation';
 
-const lookup = new Lookup();
+const iinLookup = new Lookup();
 
-export default { iinLookup: lookup, validation };
+export default { iinLookup, validation };
