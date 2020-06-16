a3ff197bc62c34ea312cbc8c8ddd6975ca0bedcd
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-06-16 23:42:18 +0200

    STJS-672 jwt generator scripts in cli.js

--- packages/jwtgenerator/cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { jwtgenerator } from './lib/jwtgenerator';
-
-const [, , ...args] = process.argv;
-
-console.log(args);
-
-// jwtgenerator(args);
---
+++ packages/jwtgenerator/cli.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { jwtgenerator } from './lib/jwtgenerator';
+import * as config from './config.json';
+
+const { payload, iss, secret } = config;
+// @ts-ignore
+const jwt = jwtgenerator(payload, secret, iss);
+console.error(jwt);
+
+
