ba41501e258dbeb8eb9b9f7c71c9159c0ad4dd1d
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-06-17 09:52:29 +0200

    STJS-672 Code amendments

--- packages/jwtgenerator/cli.ts
+++ packages/jwtgenerator/cli.ts
@@ -4,6 +4,7 @@ import { jwtgenerator } from './lib/jwtgenerator';
 import * as config from './config.json';
 
 const { payload, iss, secret } = config;
-// @ts-ignore
+
 const jwt = jwtgenerator(payload, secret, iss);
+
 console.log(jwt);
